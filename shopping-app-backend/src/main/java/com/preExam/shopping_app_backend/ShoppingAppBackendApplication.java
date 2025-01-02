23dbbac5c6677c14d7ac3080da8f82cae648fae0
unknown <42lich1mst@hft-stuttgart.de>
2025-01-02 23:42:43 +0100

    backend:latest

---
+++ shopping-app-backend/src/main/java/com/preExam/shopping_app_backend/ShoppingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.preExam.shopping_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingAppBackendApplication.class, args);
+	}
+
+}
---
+++ shopping-app-backend/src/test/java/com/preExam/shopping_app_backend/ShoppingAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preExam.shopping_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

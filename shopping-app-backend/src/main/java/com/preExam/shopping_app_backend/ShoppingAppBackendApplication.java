3659bcf0984feb6c0fa44e9feefa809a0e0851e5
Norwellin <norwellin@gmail.com>
2025-01-03 09:09:33 +0000

    backend:latest0102

--- shopping-app-backend/src/main/java/com/preExam/shopping_app_backend/ShoppingAppBackendApplication.java
+++ shopping-app-backend/src/main/java/com/preExam/shopping_app_backend/ShoppingAppBackendApplication.java
@@ -1,11 +1,20 @@
 package com.preExam.shopping_app_backend;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class ShoppingAppBackendApplication {
+	@GetMapping("/getHello")
+	public String sayHello(){
 
+		return "Hallo, Hft! (GET Call)";
+
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ShoppingAppBackendApplication.class, args);
 	}
---
+++ shopping-app-backend/src/main/java/com/preExam/shopping_app_backend/ShoppingItemRepository.java
@@ -0,0 +1,8 @@
+package com.preExam.shopping_app_backend;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface ShoppingItemRepository extends JpaRepository<ShoppingItem, Long> {
+    Optional<ShoppingItem> findByNameIgnoreCase(String name);
+}

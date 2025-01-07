1c6c9ead512800123b6322ea6f825cb24f51e688
Norwellin <norwellin@gmail.com>
2025-01-07 00:10:49 +0000

    try to fix:update

--- shopping-app-backend/src/main/java/com/hftPreExaam/preExam/PreExamApplication.java
+++ shopping-app-backend/src/main/java/com/hftPreExaam/preExam/PreExamApplication.java
@@ -1,11 +1,23 @@
 package com.hftPreExaam.preExam;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
 
 @SpringBootApplication
+@RestController
 public class PreExamApplication {
 
+	@GetMapping("/getHello")
+	public String sayHello(){
+
+		return "Hallo, Hft! (GET Call)";
+
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PreExamApplication.class, args);
 	}

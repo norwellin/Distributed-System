4dc810a8ca3f8894ebea7867f498eaacf3eebc0b
Norwellin <norwellin@gmail.com>
2025-01-07 00:01:19 +0000

    try to fix:update

---
+++ shopping-app-backend/src/main/java/com/hftPreExaam/preExam/PreExamApplication.java
@@ -0,0 +1,13 @@
+package com.hftPreExaam.preExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreExamApplication.class, args);
+	}
+
+}
---
+++ shopping-app-backend/src/test/java/com/hftPreExaam/preExam/PreExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hftPreExaam.preExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
